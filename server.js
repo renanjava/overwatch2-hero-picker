ef127d3360fffbf02c66c83575d6bfa89936271f
renanjava <renancvr777@gmail.com>
2025-04-14 16:45:54 -0300

    feat: definindo o esqueleto das rotas

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 import http from 'http'
 
+const rotas = {
+    "/": "Overwatch 2 - Hero Picker",
+    "/players": "GET ou POST para players do Overwatch 2",
+    "/players/id": "Retorna um player específico",
+    "/favorites": "Adiciona um heroi favorito ao player",
+    "/players/id/favorites": "Retorna a lista de favoritos do player",
+    "/heroes": "Retorna a lista de herois do Overwatch 2 com paginação, filtro e ordenação",
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end("Overwatch 2 - Hero Picker")
+    res.end(rotas[req.url])
 })
 
 server.listen(3000, () => {
