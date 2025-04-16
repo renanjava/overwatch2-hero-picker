57165efe666361c974943b411baed0f806321d75
Renan <renancvr777@gmail.com>
2025-04-16 01:30:08 -0300

    feat: GET com id no parâmetro

--- src/app.js
+++ src/app.js
@@ -48,41 +48,56 @@ const favorites = [
     },
 ]
 
+function buscaPlayers(idPlayer){
+    return players.findIndex((player) => player.id === idPlayer)
+}
+
+function buscaHeroes(idHero){
+    return heroes.findIndex((hero) => hero.id === idHero)
+}
+
 app.get("/", (req, res) => {
     res.status(200).send("Overwatch 2 - Hero Picker")
 })
 
-app.get("/players", (req, res) => {
+app.get("/player", (req, res) => {
     res.status(200).json(players)
 })
 
-app.post("/players", (req, res) => {
+app.post("/player", (req, res) => {
     players.push(req.body)
     res.status(201).json(players)
 })
 
-app.get("/players/1", (req, res) => {
-    res.status(200).json(players[0])
+app.get("/player/:id", (req, res) => {
+    const index = buscaPlayers(req.params.id)
+    res.status(200).json(players[index])
 })
 
-app.get("/favorites", (req, res) => {
+app.get("/favorite", (req, res) => {
     res.status(200).json(favorites)
 })
 
-app.post("/favorites", (req, res) => {
+app.post("/favorite", (req, res) => {
     favorites.push(req.body)
     res.status(201).json(favorites)
 })
 
-app.get("/players/id/favorites", (req, res) => {
-    res.status(200).json(players.heroes)
+app.get("/player/:id/favorite", (req, res) => {
+    const index = buscaPlayers(req.params.id)
+    res.status(200).json(players[index].heroes)
 })
 
-app.get("/heroes", (req, res) => {
+app.get("/hero", (req, res) => {
     res.status(200).json(heroes)
 })
 
-app.post("/heroes", (req, res) => {
+app.get("/hero/:id", (req, res) => {
+    const index = buscaHeroes(req.params.id)
+    res.status(200).json(heroes[index])
+})
+
+app.post("/hero", (req, res) => {
     heroes.push(req.body)
     res.status(201).json(heroes)
 })
