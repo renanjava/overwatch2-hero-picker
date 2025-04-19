b30f15537f031630763490967cbca579f0b2df59
Renan <renancvr777@gmail.com>
2025-04-18 23:23:26 -0300

    fix: erro de digitação

--- src/routes/heroesRoute.js
+++ src/routes/heroesRoute.js
@@ -1,7 +1,10 @@
 const { Router } = require('express')
 const HeroController = require('../controllers/HeroController')
 
+const heroController = new HeroController()
+
 const router = Router()
-router.get('/hero', HeroController.getAll)
+
+router.get('/hero', (req, res) => heroController.getAll(req, res))
 
 module.exports = router
