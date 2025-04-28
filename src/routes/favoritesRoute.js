7f5eff164d79190c728a19c7c44bf70cab56fe8f
renanjava <renancvr777@gmail.com>
2025-04-28 14:13:58 -0300

    remove: métodos não utilizáveis nas rotas de favorites e heroes

--- src/routes/favoritesRoute.js
+++ src/routes/favoritesRoute.js
@@ -8,7 +8,6 @@ const router = Router()
 router.get('/favorite', (req, res) => favoriteController.getAll(req, res));
 router.get('/favorite/:id', (req, res) => favoriteController.getOne(req, res));
 router.post('/favorite', (req, res) => favoriteController.create(req, res));
-router.put('/favorite/:id', (req, res) => favoriteController.update(req, res));
 router.delete('/favorite/:id', (req, res) => favoriteController.delete(req, res));
 
 module.exports = router
--- src/routes/heroesRoute.js
+++ src/routes/heroesRoute.js
@@ -7,8 +7,5 @@ const router = Router()
 
 router.get('/hero', (req, res) => heroController.getAll(req, res));
 router.get('/hero/:id', (req, res) => heroController.getOne(req, res));
-router.post('/hero', (req, res) => heroController.create(req, res));
-router.put('/hero/:id', (req, res) => heroController.update(req, res));
-router.delete('/hero/:id', (req, res) => heroController.delete(req, res));
 
 module.exports = router
