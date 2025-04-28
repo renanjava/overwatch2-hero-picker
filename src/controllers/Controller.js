6500f23d5646939f8a3a976443a01c18ce4d50b1
Renan <renancvr777@gmail.com>
2025-04-28 01:49:58 -0300

    feat: getById, Create, Delete e Update na Services e Controllers

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -11,6 +11,56 @@ class Controller {
 
         }
     }
+
+    async getOne(req, res) {
+        const { id } = req.params
+        try {
+            const registry = await this.entityService.getOneRegister(id)
+            if (!registry) {
+                return res.status(404).json({ message: 'Registro não encontrado' })
+            }
+            return res.status(200).json(registry)
+        } catch (erro) {
+            return res.status(500).json({ message: erro.message })
+        }
+    }
+
+    async create(req, res) {
+        const data = req.body
+        try {
+            const newRegistry = await this.entityService.createRegister(data)
+            return res.status(201).json(newRegistry)
+        } catch (erro) {
+            return res.status(500).json({ message: erro.message })
+        }
+    }
+
+    async update(req, res) {
+        const { id } = req.params
+        const data = req.body
+        try {
+            const updatedRegistry = await this.entityService.updateRegister(id, data)
+            if (!updatedRegistry) {
+                return res.status(404).json({ message: 'Registro não encontrado' })
+            }
+            return res.status(200).json(updatedRegistry)
+        } catch (erro) {
+            return res.status(500).json({ message: erro.message })
+        }
+    }
+
+    async delete(req, res) {
+        const { id } = req.params
+        try {
+            const deletedRegistry = await this.entityService.deleteRegister(id)
+            if (!deletedRegistry) {
+                return res.status(404).json({ message: 'Registro não encontrado' })
+            }
+            return res.status(204).send()
+        } catch (erro) {
+            return res.status(500).json({ message: erro.message })
+        }
+    }
 }
 
 module.exports = Controller
--- src/services/Services.js
+++ src/services/Services.js
@@ -8,6 +8,34 @@ class Services {
     async getAllRegisters() {
         return dataSource[this.nomeDoModel].findAll()
     }
+
+    async getOneRegister(id) {
+        return dataSource[this.nomeDoModel].findOne({
+            where: {
+                id: id
+            }
+        })
+    }
+
+    async createRegister(data) {
+        return dataSource[this.nomeDoModel].create(data)
+    }
+
+    async updateRegister(id, data) {
+        return dataSource[this.nomeDoModel].update(data, {
+            where: {
+                id: id
+            }
+        })
+    }
+
+    async deleteRegister(id) {
+        return dataSource[this.nomeDoModel].destroy({
+            where: {
+                id: id
+            }
+        })
+    }
 }
 
 module.exports = Services
