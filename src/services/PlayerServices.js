b36f2c3f15bdba6086a797a37388a70b5cc71bde
Renan <renancvr777@gmail.com>
2025-05-05 14:34:27 -0300

    feat: validação do body da requisição com o Yup

--- src/services/PlayerServices.js
+++ src/services/PlayerServices.js
@@ -1,8 +1,28 @@
 const Services = require('./Services')
+const Yup = require('yup');
 
 class PlayerServices extends Services {
     constructor() {
         super('Player')
+    }
+    async validatePlayerData(data) {
+        const schema = Yup.object().shape({
+            playerName: Yup.string()
+                .required('Nome do jogador é obrigatório.')
+                .min(3, 'Nome do jogador deve ter pelo menos 3 caracteres.'),
+        });
+
+        try {
+            await schema.validate(data, { abortEarly: false });
+        } catch (error) {
+            throw new Error(error.errors.join(', '));
+        }
+    }
+
+    async createRegister(data) {
+        await this.validatePlayerData(data);
+
+        return super.createRegister(data);
     }
 }
 
